c9b3ae2e7b020520e76d293887652a560e47adc2
Miguel P <bruno.pinto@rupeal,.com>
2019-05-27 22:55:32 +0100

    fix images

--- js/main.js
+++ js/main.js
@@ -123,49 +123,6 @@ $(window).scroll(function() {
 });
 
 
-/* ---- animations ---- */
-if (typeof sr == 'undefined') {
-    window.sr = ScrollReveal({
-        duration: 1600,
-        delay: 50
-    });
-}
-Royal_Preloader.config({
-    onComplete: function () {
-        triggerReveals();
-    }
-});
-function triggerReveals() {
-    sr.reveal('.bottomReveal', {
-        origin: 'bottom'
-    }).reveal('.leftReveal', {
-        origin: 'left'
-    }).reveal('.rightReveal', {
-        origin: 'right'
-    }).reveal('.topReveal', {
-        origin: 'top'
-    });
-
-    sr.reveal('.rotateBottomReveal', {
-        origin: 'bottom',
-        rotate: { x: 90 }
-    }).reveal('.rotateLeftReveal', {
-        origin: 'left',
-        rotate: { x: 90 }
-    }).reveal('.rotateRightReveal', {
-        origin: 'right',
-        rotate: { x: 90 }
-    }).reveal('.rotateTopReveal', {
-        origin: 'top',
-        rotate: { x: 90 }
-    })
-
-    sr.reveal('.scaleReveal', {
-        origin: 'top',
-        scale: 0.6
-    });
-}
-
 /* ---- close mobile nav on click ---- */
 $(document).on('click','.navbar-collapse.in',function(e) {
     if( $(e.target).is('a') && $(e.target).attr('class') != 'dropdown-toggle' ) {
